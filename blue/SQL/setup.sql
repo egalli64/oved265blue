c9f47dcf088b5579bd0233e07d8314e8870ee05c
Administrator <Administrator@192.168.12.58>
2019-09-05 11:02:14 +0200

    cars.html + images + setup.sql

--- blue/SQL/setup.sql
+++ blue/SQL/setup.sql
@@ -17,7 +17,13 @@ username varchar2(100),
 password varchar2(100));
 
 
+insert into users (user_id, email, name, creditcard, password, surname, licence, username)
+values (1, 'alfonsopappalardo@libero.it', 'alfonso', 000111222, 'informatica', 'pappalardo', 12345, 'alfopappa'  );
 
+insert into users (user_id, email, name, creditcard, password, surname, licence, username)
+values (2, 'giacomogaglione@libero.it', 'giacomo', 000123222, 'informatic', 'gaglione', 12344, 'giacgaglio'  );
 
+insert into users (user_id, email, name, creditcard, password, surname, licence, username)
+values (3, 'michelepulizzi@libero.it', 'michele', 000111223, 'informaticaa', 'pulizzi', 12355, 'michipuli'  );
 
 
