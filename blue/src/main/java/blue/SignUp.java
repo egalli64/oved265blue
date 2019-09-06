b78f71ddc22837865bcfa0660ae6a2c9d602c583
giacomogaglione <gaglione.giacomo@gmail.com>
2019-09-06 15:16:14 +0200

    signUp

--- blue/src/main/java/blue/SignUp.java
+++ blue/src/main/java/blue/SignUp.java
@@ -10,7 +10,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import javax.sql.DataSource;
-import javax.servlet.http.Cookie;
 
 @WebServlet("/SignUp")
 public class SignUp extends HttpServlet {
