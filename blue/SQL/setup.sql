3133cb3e0a5ffa5dd26103fa0bf3eb7791280d80
Administrator <Administrator@192.168.12.57>
2019-09-06 10:06:40 +0200

    setup.sql insert administrators

--- blue/SQL/setup.sql
+++ blue/SQL/setup.sql
@@ -16,22 +16,35 @@ licence varchar2(100));
 
 create sequence my_seq;
 
-create table administrator (
+create table administrators (
 administrator_id integer primary key,
 username varchar2(100),
 password varchar2(100));
 
 
 insert into users (user_id, name, last_name, username, password, email,  card_number, card_date, card_cvv, licence)
-values (my_seq.nextval, 'alfonso', 'pappalardo', 'alfopappa', 'informatica', 'alfonsopappalardo@libero.it', 000111222, 0322, 123, 'AB2411');
+values (my_seq.nextval, 'Alfonso', 'Pappalardo', 'alfopappa', 'informatica', 'alfonsopappalardo@libero.it', 000111222, 0322, 123, 'AB2411');
 
 insert into users (user_id, name, last_name, username, password, email,  card_number, card_date, card_cvv, licence)
-values (my_seq.nextval, 'giacomo', 'gaglione', 'gaglio', 'info', 'gaglione@libero.it', 002341666, 0325, 223, 'GG2411');
+values (my_seq.nextval, 'Giacomo', 'Gaglione', 'gaglio', 'info', 'gaglione@libero.it', 002341666, 0325, 223, 'GG2411');
 
 insert into users (user_id, name, last_name, username, password, email,  card_number, card_date, card_cvv, licence)
-values (my_seq.nextval, 'michele', 'pulizzi', 'michipuli', 'prova', 'michelepulizzi@libero.itt', 000111333, 4121, 113, 'MP2411');
+values (my_seq.nextval, 'Michele', 'Pulizzi', 'michipuli', 'prova', 'michelepulizzi@libero.itt', 000111333, 4121, 113, 'MP2411');
 
 
 commit;
 
+insert into administrators (administrator_id, username, password)
+values (my_seq.nextval, 'alfopappa', 'informatica');
+
+insert into administrators (administrator_id, username, password)
+values (my_seq.nextval, 'gaglio', 'info');
+
+insert into administrators (administrator_id, username, password)
+values (my_seq.nextval, 'michipuli', 'prova');
+
+insert into administrators (administrator_id, username, password)
+values (my_seq.nextval, 'egalli64', 'development');
+
+commit;
 
