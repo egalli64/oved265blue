d28e403feff5f41aa54fc55c338c50affe5bc8cc
giacomogaglione <gaglione.giacomo@gmail.com>
2019-09-10 17:48:46 +0200

    logou2

---
+++ blue/src/main/java/blue/LogoutAdmin.java
@@ -0,0 +1,38 @@
+package blue;
+
+import java.io.IOException;
+
+import javax.annotation.Resource;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.sql.DataSource;
+
+@WebServlet("/LogoutAdmin")
+public class LogoutAdmin extends HttpServlet{
+	private static final long serialVersionUID = 1L;
+	
+	@Resource(name = "jdbc/blue")
+	private DataSource ds;
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		HttpSession session = request.getSession(false);
+		session.removeAttribute("administrator");
+
+		session.getMaxInactiveInterval();
+		request.getRequestDispatcher("/LogoutAdmin.jsp").forward(request, response);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
