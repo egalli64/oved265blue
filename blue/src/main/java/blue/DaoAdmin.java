297ea3f5c1129cf4004bb9a48b18a8e3be273999
giacomogaglione <gaglione.giacomo@gmail.com>
2019-09-11 17:56:51 +0200

    adminshowtasble

--- blue/src/main/java/blue/DaoAdmin.java
+++ blue/src/main/java/blue/DaoAdmin.java
@@ -3,6 +3,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.sql.DataSource;
 public class DaoAdmin {
@@ -24,7 +26,7 @@ public class DaoAdmin {
 				PreparedStatement query = null;
 
 				String searchQuery = "select count(*) from administrators where username=? and password=?"; // ? --> parte variabile
-																									// della stringa
+																											// della stringa
 				query = conn.prepareStatement(searchQuery);
 				query.setString(1, username);
 				query.setString(2, password);// 1 --> riferito al PRIMO punto di domanda, a cui assegno la variabile stringa
@@ -46,6 +48,31 @@ public class DaoAdmin {
 				throw new IllegalStateException("Database issue " + se.getMessage());
 			}
 		}
-	}
+	
+		public List<AdminBean> showAdmins() {
+			List<AdminBean> adminList = new ArrayList<>();
+
+			try {
+				PreparedStatement showAdmin = null;
+				String select = "SELECT * FROM administrators";
+				showAdmin = conn.prepareStatement(select);
+				ResultSet rs = showAdmin.executeQuery();
+				while (rs.next()) {
+					AdminBean admin = new AdminBean();
+					
+					admin.setAdministrator_id(rs.getString(1));
+					admin.setName(rs.getString(2));
+					admin.setLast_name(rs.getString(3));
+					admin.setEmail(rs.getString(4));
+					admin.setUsername(rs.getString(5));
+					admin.setPassword(rs.getString(6));
+
+				}
+				return adminList;
+			} catch (SQLException se) {
+				throw new IllegalStateException("Database issue " + se.getMessage());
+			}
+		}
+}
 
 
